e26e9c27b79932e3f1abfa61b70a0272c2f23cb0
cacao1562 <cacao1562@naver.com>
2022-02-10 19:06:00 +0900

    Add profile type
    modify event naming

--- app.js
+++ app.js
@@ -17,55 +17,67 @@ const STATE_IDLE = 1;
 const STATE_FINDING = 2;
 const STATE_IN_ROOM = 3;
 
+const EMIT_EVENT_ADD_USER = "send add user";
+const EMIT_EVENT_FIND_USERS = "send find users";
+const EMIT_EVENT_STOP_FIND = "send stop find";
+const EMIT_EVENT_MESSAGE = "send message";
+const EMIT_EVENT_TYPING = "send typing";
+const EMIT_EVENT_NOT_TYPING = "send not typing";
+const EMIT_EVENT_EMOJI = "send emoji";
+const EMIT_EVENT_LEAVE_ROOM = "send leave room";
+
+const ON_EVENT_JOINED = "receive joined";
+const ON_EVENT_USER_FOUND = "receive user found";
+const ON_EVENT_USER_NOT_FOUND = "receive user not found";
+const ON_EVENT_USER_COUNT = "receive user count";
+const ON_EVENT_MESSAGE = "receive message";
+const ON_EVENT_IMAGES = "receive images";
+const ON_EVENT_TYPING = "receive typing";
+const ON_EVENT_NOT_TYPING = "receive not typing";
+const ON_EVENT_EMOJI = "receive emoji";
+const ON_EVENT_LEAVE_ROOM = "receive leave room";
+
 io.on('connection', (socket) => {
     // console.log('io connection');
     console.log('socket onlineUsers = ', onlineUsers);
 
-    socket.on('addUser', (data) => {
-        // response_message로 접속중인 모든 사용자에게 msg 를 담은 정보를 방출한다.
-        // io.emit('response_message', msg);
+    socket.on(EMIT_EVENT_ADD_USER, (data) => {
         const userData = JSON.parse(data);
-        // console.log('socket userData = ', userData);
         const userName = userData.userName
         const uuid = userData.uuId
-        // console.log('socket name = ', userName);
-        // console.log('socket uuid = ', uuid);
+        const shapeType = userData.shapeType
 
         socket.userName = userName;
         socket.uuId = uuid;
+        socket.shapeType = shapeType;
 
         let userInfo = {
             uuId: uuid,
             userName: userName,
             client: socket,
             roomId: "",
-            status: STATE_IDLE
+            status: STATE_IDLE,
+            shapeType: shapeType
         };
         onlineUsers.set(uuid, userInfo);
 
-        socket.emit('user joined', {
-            userName: userName,
-            numUsers: onlineUsers.size
-        });
-        console.log('>>> USER Count <<<< = ', onlineUsers.size);
-        for (let [key, value] of onlineUsers) {
-            console.log('>>> name = ', value.userName);
-        }
-        io.emit('user count', onlineUsers.size);
+        socket.emit(ON_EVENT_JOINED);
+    
+        io.emit(ON_EVENT_USER_COUNT, onlineUsers.size);
     });
 
-    socket.on('searchUser', () => {
-        // let user = onlineUsers.get(socket.uuId);
+    socket.on(EMIT_EVENT_FIND_USERS, () => {
         if (onlineUsers.size == 0) return;
         try {
-            searchUsers(socket);
+            let search = searchUsers(socket);
         }catch (err) {
+            socket.emit(ON_EVENT_USER_NOT_FOUND)
             console.log('[ERROR] = ', err);
         }
     });
 
 
-    socket.on('roomLeave', (data) => {
+    socket.on(EMIT_EVENT_LEAVE_ROOM, (data) => {
         console.log('roomLeave');
         if (onlineUsers.size == 0) return;
 
@@ -81,35 +93,42 @@ io.on('connection', (socket) => {
             if (user) {
                 user.status = STATE_IDLE;
             }
-            socket.broadcast.emit('user leave');
-
+            socket.broadcast.emit(ON_EVENT_LEAVE_ROOM);
         }
         
     });
 
-    socket.on('sendMsg', (data) => {
+    socket.on(EMIT_EVENT_MESSAGE, (data) => {
         console.log('sendMsg = ', data);
         const userMessage = JSON.parse(data);
         console.log('msg = ', userMessage.msg);
-        socket.broadcast.emit('chat message', data);
+        socket.broadcast.emit(ON_EVENT_MESSAGE, data);
     });
 
-    socket.on('typing', async () => {
-        socket.broadcast.emit('chat typing');
+    socket.on(EMIT_EVENT_TYPING, async () => {
+        socket.broadcast.emit(ON_EVENT_TYPING);
     });
-    socket.on('not typing', async () => {
-        socket.broadcast.emit('chat not typing');
+    socket.on(EMIT_EVENT_NOT_TYPING, async () => {
+        socket.broadcast.emit(ON_EVENT_NOT_TYPING);
     });
-    socket.on('send emoji', (data) => {
+    socket.on(EMIT_EVENT_EMOJI, (data) => {
         console.log('send emoji', data);
-        socket.broadcast.emit('chat emoji', data);
+        socket.broadcast.emit(ON_EVENT_EMOJI, data);
     });
 
     socket.on('disconnect', async () => {
         console.log('user disconnected');
-        socket.broadcast.emit('user leave');
-        onlineUsers.delete(socket.uuId)
-        io.emit('user count', onlineUsers.size);
+        socket.broadcast.emit(ON_EVENT_LEAVE_ROOM);
+        onlineUsers.delete(socket.uuId);
+        io.emit(ON_EVENT_USER_COUNT, onlineUsers.size);
+    });
+
+    socket.on(EMIT_EVENT_STOP_FIND, () => {
+        console.log('search stop');
+        let user = onlineUsers.get(socket.uuId);
+        if (user == undefined) return
+        if (user.status == STATE_IN_ROOM) return
+        user.status = STATE_IDLE;
     });
 
 });
@@ -122,48 +141,45 @@ function searchUsers(socket) {
             user.status = STATE_FINDING
             let count = 0;
             var interval = setInterval(function() {
-
-                if (user.status == STATE_IN_ROOM) {
+                if (user.status != STATE_FINDING) {
                     clearInterval(interval);
                 }
                 if (count > 9) {
                     user.status = STATE_IDLE;
-                    socket.emit("user not searched")
+                    socket.emit(ON_EVENT_USER_NOT_FOUND)
                     clearInterval(interval);
                 }
                 for (let [key, value] of onlineUsers) {
-                    // console.log('search key=', key, " value=" , value);
                     if (key == socket.uuId || value.status != STATE_FINDING) {
                         continue;
                     }
-                    console.log("found user key=", key)
                     const roomId = new Date().getTime()+"";
                     value.status = STATE_IN_ROOM;
                     value.roomId = roomId;
                     value.client.join(roomId);
-                    user.status = STATE_IN_ROOM
+                    user.status = STATE_IN_ROOM;
                     user.client.join(roomId);
-                    // socket.emit("user searched")
 
                     const me = {
                         uuId: socket.uuId,
                         userName: socket.userName,
-                        roomId: roomId
+                        roomId: roomId,
+                        shapeType: socket.shapeType
                     }
                     const you = {
                         uuId: value.uuId,
                         userName: value.userName,
-                        roomId: roomId
+                        roomId: roomId,
+                        shapeType: value.shapeType
                     }
-                    io.to(socket.id).emit("user searched", {
+                    io.to(socket.id).emit(ON_EVENT_USER_FOUND, {
                         userMe: me,
                         userYou: you
                     });
-                    io.to(value.client.id).emit("user searched", {
+                    io.to(value.client.id).emit(ON_EVENT_USER_FOUND, {
                         userMe: you,
                         userYou: me
                     });
-                    // io.sockets.to(roomId).emit("user searched", data);
                     clearInterval(interval);
                     break;
                 }
@@ -182,14 +198,13 @@ function searchUsers(socket) {
 const upload = multer({ dest: 'uploads/', limits: { fileSize: 5 * 1024 * 1024 } });
 
 
-// app.get('/uploads/:upload', function(req, res) {
-//     var file = req.params.upload;
-//     console.log(file);
-//     var img = fs.readFileSync(__dirname + "/uploads/" + file);
-
-//     res.writeHead(200, {'Content-Type': 'image/png'});
-//     res.end(img, 'binary');
-// });
+app.get('/uploads/:upload', function(req, res) {
+    var file = req.params.upload;
+    console.log(file);
+    var img = fs.readFileSync(__dirname + "/uploads/" + file);
+    res.writeHead(200, {'Content-Type': 'image/png'});
+    res.end(img, 'binary');
+});
 
 // image라는 폴더경로로 node서버에 있는 uploads폴더 매핑
 // 클라이언트에서는 uploads로 접근못하고 image로 접근하도록
@@ -214,7 +229,7 @@ app.post('/uploadImg', upload.array('upload', 3), async (req, res, next) => {
                 state: 200,
                 imgPath: pathList
             });
-            io.to(`${req.body.roomId}`).emit('chat image', {
+            io.to(`${req.body.roomId}`).emit(ON_EVENT_IMAGES, {
                 uuId: req.body.uuId,
                 date: date,
                 imageNames: pathList
@@ -225,13 +240,3 @@ app.post('/uploadImg', upload.array('upload', 3), async (req, res, next) => {
         next(error);
     }    
 });
-
-// TEST CODE GOES HERE
-// (async function(){
-// })();
-
-
-
-// http.listen(3000, () => {
-//     console.log('Connected at 3000');
-// });
